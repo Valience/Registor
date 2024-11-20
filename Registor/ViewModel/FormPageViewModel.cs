e477601d86c8c6e0737991053fa31634f74143f1
Vitali <basaraba.vitalii@chnu.edu.ua>
2024-11-20 18:54:01 +0200

    fix #1

--- Registor/ViewModel/FormPageViewModel.cs
+++ Registor/ViewModel/FormPageViewModel.cs
@@ -2,7 +2,6 @@
 using CommunityToolkit.Mvvm.Input;
 using Registor.Model;
 using Registor.Services;
-using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
 
